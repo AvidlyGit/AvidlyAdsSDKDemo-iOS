347f29e87ff820f801e7aa48a1d4b08dab894d12
steve <steve.liu@holaverse.com>
2017-07-05 10:13:24 +0800

    2.0.23 build 2

--- Framework/AvidlyAdsSDK/AvidlyAdsSDK.framework/Headers/AvidlyAdsSDK.h
+++ Framework/AvidlyAdsSDK/AvidlyAdsSDK.framework/Headers/AvidlyAdsSDK.h
@@ -32,4 +32,18 @@
  */
 + (void)initSDK:(AvidlyAdsGlobalZone)zone;
 
+/**
+ 获取广告配置
+ 
+ @param gameAccountId 用户在游戏中的帐号id
+ @param completeTask 是否完成了游戏中的新手任务，可选0或者1
+ @param isPaid 付费 - 0 未付费；>1 付费金额
+ @param promotionChannelName 推广渠道
+ @param gender 性别   "M", "F"，未知可以传""
+ @param age 年龄
+ @param tags 标签
+ @return 广告展示策略
+ */
++ (NSDictionary *)getAdConfigWithGameAccountId:(NSString *)gameAccountId completeTask:(BOOL)completeTask isPaid:(int)isPaid promotionChannelName:(NSString *)promotionChannelName gender:(NSString *)gender age:(int)age tags:(NSArray<NSString *> *)tags;
+
 @end
